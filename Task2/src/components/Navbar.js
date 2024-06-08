d2aa86f60fc30f1c74d4bbb2c8d29ec2da462096
Akshay s nair <94843189+Akshay-s-nair@users.noreply.github.com>
2024-06-08 12:09:03 +0530

    Add files via upload

---
+++ Task2/src/components/Navbar.js
@@ -0,0 +1,14 @@
+import 'D:/Akshay/VSCode/React_projects/innovaturetask2/src/Navbar.css'
+export default function Navbar(){
+    return(
+        <nav className="nav-container">
+        
+            <h2 className="nav-title">EMP management</h2>
+            <div className="home-link">
+    
+                <h6><a href="/" className="home-links">Log Out</a></h6>
+            </div>
+        </nav>
+
+    )
+}
